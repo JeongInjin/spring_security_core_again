fa4463f785e881da3030e9a4818d8374531e775c
injinjeong <snowwwwww@naver.com>
2022-02-21 21:44:58 +0900

    again security core init

---
+++ src/main/java/io/security/corespringsecurity/spring_security_core_again/SpringSecurityCoreAgainApplication.java
@@ -0,0 +1,13 @@
+package io.security.corespringsecurity.spring_security_core_again;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCoreAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityCoreAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/security/corespringsecurity/spring_security_core_again/SpringSecurityCoreAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.corespringsecurity.spring_security_core_again;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCoreAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
